3ecd26912bb5418972aaab858f94eb4c7baaa4da
Sudhir Verma <SU20443679@wipro.com>
2022-12-19 09:45:44 +0530

    Added program for easy section of graph

---
+++ leetcode/graph/easy/1791.-Find-Center-of-Star-Graph.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/find-center-of-star-graph/
+
+var findCenter = function(edges) {
+    let map = {};
+    for (let i=0; i<edges.length; i++) {
+        if (map[edges[i][0]] === undefined) map[edges[i][0]] = 1;
+        else
+        map[edges[i][0]]++;
+
+        if (map[edges[i][1]] === undefined) map[edges[i][1]] = 1; 
+        else
+        map[edges[i][1]]++;
+        if (map[edges[i][1]] === edges.length-1) return edges[i][1];
+        if (map[edges[i][0]] === edges.length-1) return edges[i][0];
+    }
+};
+
+let edges = [[1,2],[2,3],[4,2]];
+console.log(findCenter(edges));
---
+++ leetcode/graph/easy/997.-Find-the-Town-Judge.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/find-the-town-judge/
+
+var findJudge = function(n, trust) {
+    let map = {};
+    for (let i=1; i<=n; i++) {
+        map[i] = 0;
+    }
+    for (let i=0; i<trust.length; i++) {
+        map[trust[i][0]] -= 1;
+        map[trust[i][1]] += 1;
+    }
+    let key = Object.keys(map);
+    for (let i=0; i<key.length; i++) {
+        if (map[key[i]] === n-1) {
+            console.log(map[key[i]])
+            return parseInt(key[i]);
+        }
+    }
+    return -1;
+}
+
+let n = 2, trust = [[1,2]];
+console.log(findJudge(n, trust));
